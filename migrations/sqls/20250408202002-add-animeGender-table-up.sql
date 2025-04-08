e1317c6bd028675ff316480348bca2e89659323a
Luis Jair <naopubg28@gmail.com>
2025-04-08 15:10:04 -0600

    change name of gender table

--- migrations/sqls/20250408202002-add-animeGender-table-up.sql
+++ migrations/sqls/20250408202002-add-animeGender-table-up.sql
@@ -2,7 +2,7 @@ SET
     search_path to public;
 
 CREATE TABLE
-    IF NOT EXISTS tbl_gender (
+    IF NOT EXISTS tbl_genders (
         id SERIAL PRIMARY KEY,
         name varchar(255) NOT NULL,
         delete boolean DEFAULT false
@@ -17,4 +17,4 @@ CREATE TABLE
 
 ALTER TABLE tbl_anime_gender ADD CONSTRAINT fk_anime FOREIGN KEY (id_anime) REFERENCES tbl_animes (id);
 
-ALTER TABLE tbl_anime_gender ADD CONSTRAINT fk_gender FOREIGN KEY (id_gender) REFERENCES tbl_gender (id);
+ALTER TABLE tbl_anime_gender ADD CONSTRAINT fk_gender FOREIGN KEY (id_gender) REFERENCES tbl_genders (id);
