56c8a3692ea6f3e5f79a546139f9f780d5788218
Luis Jair <naopubg28@gmail.com>
2025-04-09 01:34:27 -0600

    add upload studios and animes data

--- src/controllers/upload-masive-data.controller.js
+++ src/controllers/upload-masive-data.controller.js
@@ -2,6 +2,12 @@ const axios = require("axios");
 const HandlerError = require("../helpers/handlerError.helper");
 const ApiResponse = require("../helpers/reponse-api.helper");
 const Gender = require("../models/gender.model");
+const Studio = require("../models/studio.model");
+const Animes = require("../models/anime.model");
+const AnimeGender = require("../models/anime-gender.model");
+const AnimeStudio = require("../models/anime-studio.model");
+const Anime = require("../models/anime.model");
+
 const uploadGenders = async (req, res) => {
   try {
     const gendersApiResponse = await axios.get(
@@ -13,10 +19,92 @@ const uploadGenders = async (req, res) => {
     }
     return ApiResponse(res, "Se cargaron los generos");
   } catch (error) {
-    HandlerError(res, error);
+    return HandlerError(res, error);
+  }
+};
+
+const uploadStudiosMasive = async (req, res) => {
+  try {
+    const { page } = req.query;
+    const animeApiResponse = await axios.get(
+      `https://api.jikan.moe/v4/anime?page=${page}`
+    );
+    const { data } = animeApiResponse.data;
+
+    for (const anime of data) {
+      for (const studio of anime.studios) {
+        const existStudio = await Studio.findOne({
+          where: {
+            name: studio.name,
+          },
+        });
+        if (!existStudio) {
+          await Studio.create({
+            name: studio.name,
+          });
+        }
+      }
+    }
+    return ApiResponse(res, "Carga de estudios completada");
+  } catch (error) {
+    return HandlerError(res, error);
+  }
+};
+
+const uploadAnimeMasive = async (req, res) => {
+  try {
+    const { page } = req.query;
+    const animeApiResponse = await axios.get(
+      `https://api.jikan.moe/v4/anime?page=${page}`
+    );
+    const { data } = animeApiResponse.data;
+
+    for (const anime of data) {
+      const newAnime = await Anime.create({
+        title: anime.title,
+        description: anime.synopsis,
+        type: anime.type,
+        language: "Japanese",
+        durationEpisodes: anime.duration,
+        emitted: anime.aired.from,
+        state: "Finished",
+      });
+
+      for (const gender of anime.genres) {
+        const foundGender = await Gender.findOne({
+          where: {
+            name: gender.name,
+          },
+        });
+        if (foundGender) {
+          AnimeGender.create({
+            idAnime: newAnime.id,
+            idGender: foundGender.id,
+          });
+        }
+      }
+      for (const studio of anime.studios) {
+        const foundStudio = await Studio.findOne({
+          where: {
+            name: studio.name,
+          },
+        });
+        if (foundStudio) {
+          AnimeStudio.create({
+            idAnime: newAnime.id,
+            idStudio: foundStudio.id,
+          });
+        }
+      }
+    }
+    return ApiResponse(res, "Carga de animes chido");
+  } catch (error) {
+    return HandlerError(res, error);
   }
 };
 
 module.exports = {
   uploadGenders,
+  uploadAnimeMasive,
+  uploadStudiosMasive,
 };
--- src/routes/upload-masive-data.route.js
+++ src/routes/upload-masive-data.route.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const {
   uploadGenders,
+  uploadAnimeMasive,
+  uploadStudiosMasive,
 } = require("../controllers/upload-masive-data.controller");
 const route = Router();
 
 route.get("/upload-gender", uploadGenders);
+route.get("/upload-animes", uploadAnimeMasive);
+route.get("/upload-studios", uploadStudiosMasive);
 module.exports = route;
